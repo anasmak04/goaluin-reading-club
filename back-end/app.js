13f539dc9743ac629ab436fa0c0a35f573b36392
anas elmakhloufi <anasdev8@gmail.com>
2023-06-09 15:19:14 +0100

    Implement login and registration functionality

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const connectDb = require("./config/DbConnection");
 const dotenv = require("dotenv").config();
+const ErrorHandler = require("./middleware/ErrorHandler");
 const app = express();
 connectDb();
 app.use(express.json());
 
 app.use("/api/user", require("./routes/UserRouter"));
 
-
+app.use(ErrorHandler);
 
 
 const port = process.env.PORT
--- back-end/controller/UserController.js
+++ back-end/controller/UserController.js
@@ -1,11 +1,64 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/UserModel")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are necessary!");
+    }
+    const checkUser = await User.findOne({ email });
+    if (checkUser) {
+        res.status(400);
+        throw new Error("User already registered!");
+    }
 
 
-const registerUser = (req, res) => {
-    res.json({ message: "register a user" })
-}
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed Password: ", hashedPassword);
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    });
 
-const loginUser = (req, res) => {
-    res.json({ message: "login a user" })
-}
+    console.log(`User created ${user}`);
+    if (user) {
+        res.status(200).json({ _id: user.id, email: user.email });
+    } else {
+        res.status(400);
+        throw new Error("User is not valid");
+    }
+    res.json({ message: "Register the user" });
+})
+
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are necessary!");
+    }
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            {
+                user: {
+                    username: user.username,
+                    email: user.email,
+                    id: user.id,
+                },
+            },
+            process.env.TOKEN_SECRET,
+            { expiresIn: "20m" }
+        );
+        res.status(200).json({ accessToken });
+    } else {
+        res.status(401);
+        throw new Error("email or password is not valid");
+    }
+
+});
 
 module.exports = { registerUser, loginUser };
