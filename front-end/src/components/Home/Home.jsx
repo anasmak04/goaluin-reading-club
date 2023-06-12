31db92e56024f77b9382c44ec947d04de1464f04
anas elmakhloufi <anasdev8@gmail.com>
2023-06-12 15:39:21 +0100

    update post ui

--- front-end/src/components/Home/Home.jsx
+++ front-end/src/components/Home/Home.jsx
@@ -1,42 +1,51 @@
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import "./Home.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
+
+
+
   const [data, setData] = useState([]);
- 
-useEffect(() => {
-  const getData = async () => {
-    const response = await fetch(`http://localhost:7000/api/user/post`);
-    const postdata = await response.json();
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(`http://localhost:7000/api/user/post`);
+      const postdata = await response.json();
       console.log(postdata)
-    setData(postdata);
-  }
+      setData(postdata);
+    }
 
-  getData();
-},[])
+    getData();
+  }, [])
 
-  
-  
+
+
   return (
     <div className="container">
-   
-        {data.map((item) => {
-          return (
-            <div className="box">
-              <div>
-              <h1 key={item.id}>{item.title}</h1><br />
-            <h2>{item.description}</h2>
-              </div>
 
-              <div>
-              <FontAwesomeIcon  icon={faTrash} className="icon" />
-              </div>
+      {data.map((item) => {
+        return (
+          <div className="box">
+            <div>
+              <h1 key={item.id}>{item.title}</h1><br />
+              <h2>{item.description}</h2>
+            
+              <h3>  <FontAwesomeIcon icon={faCalendar} className="icone" /> {new Date().toLocaleString() + ''}</h3>
             </div>
-          )
-        })}
-     
+
+            <div>
+              <FontAwesomeIcon icon={faTrash} className="icon" />
+            </div>
+
+            
+
+          </div>
+        )
+      })}
+
     </div>
   )
 }
